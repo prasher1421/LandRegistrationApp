159d7b3fe5c00519f95a9dfb7f8cf87c9102dc1b
prasher1421 <aryan1421g@gmail.com>
2023-02-03 18:21:48 +0530

    Fragments_Created

--- app/src/main/java/com/prasher/landregistration/MainActivity.kt
+++ app/src/main/java/com/prasher/landregistration/MainActivity.kt
@@ -2,10 +2,39 @@ package com.prasher.landregistration
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.prasher.landregistration.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding : ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding?.root)
+        replaceFragment(Home())
+
+        binding?.navBar?.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(Home())
+                R.id.about -> replaceFragment(About())
+                R.id.me -> replaceFragment(User())
+                R.id.contact -> replaceFragment(ContactOwner())
+                R.id.inspect -> replaceFragment(InspectLand())
+
+                else -> {}
+            }
+            true
+        }
+
     }
+
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+    }
+
 }
