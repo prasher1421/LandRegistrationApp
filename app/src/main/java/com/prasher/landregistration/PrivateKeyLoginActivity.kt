df1beb974ea8af7cc96b701b8b9f2f3adf954846
unknown <aryan1421g@gmail.com>
2023-03-14 23:35:49 +0530

    Home UI Complete

---
+++ app/src/main/java/com/prasher/landregistration/PrivateKeyLoginActivity.kt
@@ -0,0 +1,20 @@
+package com.prasher.landregistration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.prasher.landregistration.databinding.ActivityPrivateKeyLoginBinding
+
+class PrivateKeyLoginActivity : AppCompatActivity() {
+
+    private var binding : ActivityPrivateKeyLoginBinding?=null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityPrivateKeyLoginBinding.inflate(layoutInflater)
+
+
+
+        super.onCreate(savedInstanceState)
+        setContentView(binding?.root)
+    }
+}
