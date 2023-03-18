c11cf6453102bdd59fb162475beabb461e3029bd
unknown <aryan1421g@gmail.com>
2023-03-18 15:08:01 +0530

    User Page Started

--- app/src/main/java/com/prasher/landregistration/SignUpActivity.kt
+++ app/src/main/java/com/prasher/landregistration/SignUpActivity.kt
@@ -1,11 +1,22 @@
 package com.prasher.landregistration
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.prasher.landregistration.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    private var binding : ActivitySignUpBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+
+        binding?.btnContinue?.setOnClickListener {
+            val intent = Intent(this,UserActivity::class.java)
+            startActivity(intent)
+        }
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(binding?.root)
     }
 }
